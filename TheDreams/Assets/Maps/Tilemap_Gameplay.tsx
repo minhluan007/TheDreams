e53447e1ab5b5fe9ea2c659b539e3cf33b32968b
minhluan007 <luanjoedesign@gmail.com>
2026-07-02 21:36:59 +0700

    02/07/2026
    Máp1

---
+++ TheDreams/Assets/Maps/Tilemap_Gameplay.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Tilemap_Gameplay" tilewidth="16" tileheight="16" tilecount="357" columns="17">
+ <image source="maps-art.png" width="272" height="336"/>
+</tileset>
